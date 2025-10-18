874548a5d2eccbb1f51c0aa7d5b67189d9327e93
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-10-19 00:22:21 +0100

    Add requirements.txt

--- TamTechwebSite/TamTechwebSite/urls.py
+++ TamTechwebSite/TamTechwebSite/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from .views import home
+from base.views import home
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
