437d0362532ab1cf8fe9baa16914262ad7a394f9
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-10-24 03:21:15 +0100

    Add missing config.js for CloudImage

--- Client/src/config/config.js
+++ Client/src/config/config.js
@@ -18,10 +18,7 @@ const CONFIG = {
     ? "http://127.0.0.1:8000/media/"
     : "https://jorfofdjangov.onrender.com/media/",
 
-  API_PHOTO_LIST: BASE_URL.includes("127.0.0.1")
-    ? "http://127.0.0.1:8000/media/"
-    : "https://jorfofdjangov.onrender.com/media/",
-
+  API_PHOTO_LIST: `${BASE_URL}/api/media/`,
   CLOUDINARY_NAME: "dwuyq2eoz", // ⚡ remplace par ton cloud name exact
 
   
