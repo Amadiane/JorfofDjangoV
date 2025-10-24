048bdd0da50a0f41623bc75e4049086841715afc
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-10-24 03:11:20 +0100

    Add missing config.js for CloudImage

--- Client/src/config/config.js
+++ Client/src/config/config.js
@@ -1,46 +1,30 @@
-// // const BASE_URL = "http://127.0.0.1:8000"; // ✅ d’abord on la définit ici
-// // ✅ Détection automatique : local ou production
-// const BASE_URL =
-//   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-//     ? "http://127.0.0.1:8000"
-//     : "https://jorfofdjangov.onrender.com"; // ton URL Render
-
-// const CONFIG = {
-//   BASE_URL: "http://127.0.0.1:8000", // ton backend local
-//   API_LOGIN: "/api/login/",
-
-//   //   // 🖼️ Photothèque
-//   API_PHOTO_LIST: `${BASE_URL}/api/phototheque/`,
-//   API_PHOTO_DETAIL: (id) => `${BASE_URL}/api/phototheque/${id}/`,
-
-//   //   // 📸 Images (chemin vers le dossier media)
-//   MEDIA_URL: `${BASE_URL}/media/`,
-//   API_PHOTO_LIST: `${BASE_URL}/api/media/`,
-//   CLOUDINARY_NAME: "dwuyq2eoz", // ⚡ remplace par ton cloud name exact
-
-  
-
-// };
-
-// export default CONFIG;
-
-
 // const BASE_URL = "http://127.0.0.1:8000"; // ✅ d’abord on la définit ici
 // ✅ Détection automatique : local ou production
 const BASE_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:8000"
-    : "https://jorfofdjangov.onrender.com";
+    : "https://jorfofdjangov.onrender.com"; // ton URL Render
 
 const CONFIG = {
-  BASE_URL,
-  API_LOGIN: `${BASE_URL}/api/login/`,
+  BASE_URL: "http://127.0.0.1:8000", // ton backend local
+  API_LOGIN: "/api/login/",
 
-  API_PHOTO_LIST: `${BASE_URL}/api/phototheque/`,
-  API_PHOTO_DETAIL: (id) => `${BASE_URL}/api/phototheque/${id}/`,
+  //   // 🖼️ Photothèque
+  API_PHOTO_LIST: `${BASE_URL}/api/media/`,
+  API_PHOTO_DETAIL: (id) => `${BASE_URL}/api/media/${id}/`,
 
-  MEDIA_URL: `${BASE_URL}/media/`, // utile seulement si backend renvoie des chemins relatifs
-  CLOUDINARY_NAME: "dwuyq2eoz",
+  //   // 📸 Images (chemin vers le dossier media)
+  MEDIA_URL: `${BASE_URL}/media/`,
+  API_PHOTO_LIST: `${BASE_URL}/api/media/`,
+  CLOUDINARY_NAME: "dwuyq2eoz", // ⚡ remplace par ton cloud name exact
+
+  
+
 };
 
 export default CONFIG;
+
+
+// const BASE_URL = "http://127.0.0.1:8000"; // ✅ d’abord on la définit ici
+// ✅ Détection automatique : local ou production
+
